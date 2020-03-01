d3c1de0226ff6f8cc6f90c5fd25c6f0f213f1606
dmfananddu <af0706@yandex.ru>
2020-03-01 19:22:04 +0300

    Parser.py modified + readme.md modified due to parser.py

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
 from pymorphy2 import MorphAnalyzer
 
-slovo = "е"
+slovo = input().strip()
 morph = MorphAnalyzer().parse(slovo)
 wordCount = 0
 
 for i in range(len(morph)):
-    if morph[i].normal_form == slovo.lower() or len(morph[i].normal_form)//len(slovo.lower()) < 3:
+    if (morph[i].normal_form == slovo.lower() or len(morph[i].normal_form)//len(slovo.lower()) < 3) and {'UNKN'} not in morph[i].tag:
         wordCount += 1
 print(wordCount)
 print(morph)
--- parser.py
+++ parser.py
@@ -11,34 +11,58 @@ def wordExistingCheck(buf):
     wordCount = 0
     morph = MorphAnalyzer().parse(buf)
     for i in range(len(morph)):
-        if morph[i].normal_form == buf.lower() or len(morph[i].normal_form)//len(buf.lower()) < 3:
+        if (morph[i].normal_form == buf.lower() or len(morph[i].normal_form)//len(buf.lower()) < 3) and {'UNKN'} not in morph[i].tag:
             wordCount += 1
     return wordCount
 
 for symbol in inputStr:
     buf += symbol
-    if 48 <= ord(symbol) <= 57:
-        print("цифра:", symbol)
-    elif 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or 91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 127:
-        print("знак препинания:", symbol)
-    elif 65 <= ord(symbol) <= 90:
-        print("Прописная латинская:", symbol)
-    elif 97 <= ord(symbol) <= 122:
-        print("Строчная латинская:", symbol)
-    elif 1040 <= ord(symbol) <= 1071 or ord(symbol) == 1025:
-        print("Прописная русская:", symbol)
-    elif 1072 <= ord(symbol) <= 1103 or ord(symbol) == 1105:
-        print("Строчная русская:", symbol)
-    elif ord(symbol) == 32:
+    # if 48 <= ord(symbol) <= 57:
+    #     print("цифра:", symbol)
+    # elif 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or 91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 127:
+    #     print("знак препинания:", symbol)
+    # elif 65 <= ord(symbol) <= 90:
+    #     print("Прописная латинская:", symbol)
+    # elif 97 <= ord(symbol) <= 122:
+    #     print("Строчная латинская:", symbol)
+    # elif 1040 <= ord(symbol) <= 1071 or ord(symbol) == 1025:
+    #     print("Прописная русская:", symbol)
+    # elif 1072 <= ord(symbol) <= 1103 or ord(symbol) == 1105:
+    #     print("Строчная русская:", symbol)
+    if ord(symbol) == 32:
         buf = buf[:-1]
-        if buf != "" and ord(buf[-1]) != 46:
-            print(buf)
-            stringList.append(buf)
-            buf = ""
-        elif len(buf) >= 1:
-            if wordExistingCheck(buf[:-1]) != 0:
+        if buf == "":
+            continue
+        if ord(buf[-1]) != 46:
+            print("не точка", stringList)
+            if wordExistingCheck(buf) != 0:
                 stringList.append(buf)
                 buf = ""
+        elif wordExistingCheck(buf[:-1]) != 0:
+            print("точка", stringList)
+            if len(buf) > 2:
+                stringList.append(buf)
+                buf = ""
+            elif len(buf) == 1:
+                stringList[-1] += buf
+                buf = ""
+        else:
+            if len(buf) == 1:
+                stringList[-1] += buf
+                buf = ""
 if buf != "":
-    stringList.append(buf)
+    if ord(buf[-1]) != 46 and wordExistingCheck(buf) != 0:
+        stringList.append(buf)
+        buf = ""
+    elif wordExistingCheck(buf[:-1]) != 0:
+        if len(buf) > 2:
+            stringList.append(buf)
+            buf = ""
+        elif len(buf) == 1:
+            stringList[-1] += buf
+            buf = ""
+    else:
+        if len(buf) == 1:
+            stringList[-1] += buf
+            buf = ""
 print(stringList)
