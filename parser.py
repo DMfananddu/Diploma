7557460aeb6bfe3f8cee26152933d5e48e21ac77
dmfananddu <af0706@yandex.ru>
2020-03-11 12:46:15 +0300

    Parser.py (added memory_profiler view for further comparising with nltk) + nltk_using.py add as main Parser

--- parser.py
+++ parser.py
@@ -1,13 +1,32 @@
 from pymorphy2 import MorphAnalyzer
 from copy import deepcopy
-# ввод текста вручную в командной строке
-inputStr = input("Введите текст, оканчивающийся символом переноса строки (или нажмите Enter): ").strip("\n")
 
-# ввод текста чтением из файла
+# выбор сповоба ввода текста:
+inputStr = ""
+inputFile = []
+while True:
+    choiceInputStr = input("Если хотите ввести строку текста в командной строке, введите слово 'вручную',\nесли хотите прочитать текст из файла, введите слово 'файл': ").strip("\n")
+    if choiceInputStr != "файл" and choiceInputStr != "вручную":
+        print("Пожалуйста, введите корректную инструкцию для продолжения работы.\n")
+        continue
+    elif choiceInputStr != "файл" and choiceInputStr == "вручную":
+        # ввод текста вручную в командной строке
+        inputStr = input("Введите текст, оканчивающийся символом переноса строки (или нажмите Enter): ").strip("\n")
+        break
+    else:
+        # ввод текста чтением из файла
+        inputFileName = input("Введите имя файла (полный путь/имя внутри данной директории): ").strip("\n")
+        f = open(inputFileName, encoding="UTF-8").readlines()
+        for line in f:
+            inputFile.append(line.strip("\n"))
+        print(inputFile)
+        break
 
-stringList = []
-buf = ""
 
+inputData = inputStr or inputFile
+dataWordsList = []
+
+@profile
 def wordExistingCheck(buf):
     wordCount = 0
     morph = MorphAnalyzer().parse(buf)
@@ -16,7 +35,8 @@ def wordExistingCheck(buf):
             wordCount += 1
     return wordCount
 
-def wordProcessing(buf):
+@profile
+def wordProcessing(buf, stringList):
     if ord(buf[-1]) == 46:
         # print("точка", buf, stringList)
         if wordExistingCheck(buf[:-1]) != 0:
@@ -42,75 +62,82 @@ def wordProcessing(buf):
         else:
             stringList.append(buf)
             buf = ""
-    return buf
+    return buf, stringList
 
-for symbol in inputStr:
-    buf += symbol
-    # if 48 <= ord(symbol) <= 57:
-    #     print("цифра:", symbol)
-    # elif 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or 91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 127:
-    #     print("знак препинания:", symbol)
-    # elif 65 <= ord(symbol) <= 90:
-    #     print("Прописная латинская:", symbol)
-    # elif 97 <= ord(symbol) <= 122:
-    #     print("Строчная латинская:", symbol)
-    # elif 1040 <= ord(symbol) <= 1071 or ord(symbol) == 1025:
-    #     print("Прописная русская:", symbol)
-    # elif 1072 <= ord(symbol) <= 1103 or ord(symbol) == 1105:
-    #     print("Строчная русская:", symbol)
-    if ord(symbol) == 32:
-        buf = buf[:-1]
-        if buf == "":
-            continue
-        buf = wordProcessing(buf)
-if buf != "":
-    buf = wordProcessing(buf)
-if buf != "":
-    stringList.append(buf)
+@profile
+def dataSlicing(inputData):
     buf = ""
+    for dataStr in inputData:
+        dataWordsList.append([])
+        for symbol in dataStr:
+            buf += symbol
+            # if 48 <= ord(symbol) <= 57:
+            #     print("цифра:", symbol)
+            # elif 33 <= ord(symbol) <= 47 or 58 <= ord(symbol) <= 64 or 91 <= ord(symbol) <= 96 or 123 <= ord(symbol) <= 127:
+            #     print("знак препинания:", symbol)
+            # elif 65 <= ord(symbol) <= 90:
+            #     print("Прописная латинская:", symbol)
+            # elif 97 <= ord(symbol) <= 122:
+            #     print("Строчная латинская:", symbol)
+            # elif 1040 <= ord(symbol) <= 1071 or ord(symbol) == 1025:
+            #     print("Прописная русская:", symbol)
+            # elif 1072 <= ord(symbol) <= 1103 or ord(symbol) == 1105:
+            #     print("Строчная русская:", symbol)
+            if ord(symbol) == 32:
+                buf = buf[:-1]
+                if buf == "":
+                    continue
+                buf, dataWordsList[-1] = wordProcessing(buf, dataWordsList[-1])
+    if buf != "":
+        buf, dataWordsList[-1] = wordProcessing(buf, dataWordsList[-1])
+    if buf != "":
+        dataWordsList[-1].append(buf)
+        buf = ""
+    return dataWordsList
 
+dataWordsList = dataSlicing(inputData)
 resultList = []
 firstLetterIndex = 0
-print(stringList)
+print(dataWordsList)
 
-for stringElem in stringList:
-    elemLength = len(stringElem)
-    elem = deepcopy(stringElem)
-    if len(elem) != 1:
-        if 33 <= ord(elem[0]) <= 47 or 58 <= ord(elem[0]) <= 64 or \
-                91 <= ord(elem[0]) <= 96 or 123 <= ord(elem[0]) <= 127 and \
-                33 <= ord(elem[-1]) <= 47 or 58 <= ord(elem[-1]) <= 64 or \
-                91 <= ord(elem[-1]) <= 96 or 123 <= ord(elem[-1]) <= 127:
-            for i in range(elemLength-1):
-                if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
-                    print(elem[i], elem)
-                    print(i, elemLength)
-                    resultList.append(elem[:i])
-                    elem = elem[i:]
-                    elemLength = len(elem)
+@profile
+def dataFianlPacking(dataWordsList):
+    for stringList in dataWordsList:
+        for stringElem in stringList:
+            elemLength = len(stringElem)
+            elem = deepcopy(stringElem)
+            if len(elem) != 1:
+                if 33 <= ord(elem[0]) <= 47 or 58 <= ord(elem[0]) <= 64 or \
+                        91 <= ord(elem[0]) <= 96 or 123 <= ord(elem[0]) <= 127 and \
+                        33 <= ord(elem[-1]) <= 47 or 58 <= ord(elem[-1]) <= 64 or \
+                        91 <= ord(elem[-1]) <= 96 or 123 <= ord(elem[-1]) <= 127:
+                    for i in range(elemLength-1):
+                        if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
+                            resultList.append(elem[:i])
+                            elem = elem[i:]
+                            elemLength = len(elem)
+                            for i in range(elemLength-1, 0, -1):
+                                if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
+                                    resultList.append(elem[:i+1])
+                                    resultList.append(elem[i+1:])
+                                    break
+                            break
+                elif 33 <= ord(elem[0]) <= 47 or 58 <= ord(elem[0]) <= 64 or 91 <= ord(elem[0]) <= 96 or 123 <= ord(elem[0]) <= 127:
+                    for i in range(elemLength-1):
+                        if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
+                            resultList.append(elem[:i])
+                            resultList.append(elem[i:])
+                            break
+                elif 33 <= ord(elem[-1]) <= 47 or 58 <= ord(elem[-1]) <= 64 or 91 <= ord(elem[-1]) <= 96 or 123 <= ord(elem[-1]) <= 127:
                     for i in range(elemLength-1, 0, -1):
                         if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
                             resultList.append(elem[:i+1])
                             resultList.append(elem[i+1:])
                             break
-                    break
-        elif 33 <= ord(elem[0]) <= 47 or 58 <= ord(elem[0]) <= 64 or 91 <= ord(elem[0]) <= 96 or 123 <= ord(elem[0]) <= 127:
-            for i in range(elemLength-1):
-                if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
-                    print(elem[i], elem)
-                    print(i, elemLength)
-                    resultList.append(elem[:i])
-                    resultList.append(elem[i:])
-                    break
-        elif 33 <= ord(elem[-1]) <= 47 or 58 <= ord(elem[-1]) <= 64 or 91 <= ord(elem[-1]) <= 96 or 123 <= ord(elem[-1]) <= 127:
-            for i in range(elemLength-1, 0, -1):
-                if not (33 <= ord(elem[i]) <= 47 or 58 <= ord(elem[i]) <= 64 or 91 <= ord(elem[i]) <= 96 or 123 <= ord(elem[i]) <= 127):
-                    print(elem[i], elem)
-                    resultList.append(elem[:i+1])
-                    resultList.append(elem[i+1:])
-                    break
-        else:
-            resultList.append(elem)            
-    else:
-        resultList.append(elem)
+                else:
+                    resultList.append(elem)            
+            else:
+                resultList.append(elem)
+    return resultList
+resultList = dataFianlPacking(dataWordsList)
 print(resultList)
