439a4f4b6bfd8bc9a03b66a7edf49d021aeed32a
dmfananddu <af0706@yandex.ru>
2020-03-26 19:28:16 +0300

    readme.md updated

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ wordCount = 0
 
 for i in range(len(morph)):
     if morph[i].normal_form == slovo.lower() or len(morph[i].normal_form)//len(slovo.lower()) < 3 and {'UNKN'} not in morph[i].tag:
-        print(morph[i].normal_form, slovo.lower())
+        # print(morph[i].normal_form, slovo.lower())
         wordCount += 1
 print(wordCount)
 print(morph)
--- nltk_parser.py
+++ nltk_parser.py
@@ -1,5 +1,20 @@
 import nltk
-nltk.download()
 from reader import gettingData
+from pymorphy2 import MorphAnalyzer
 
-print(0)
+text = input().strip()
+
+sentences = nltk.sent_tokenize(text)
+for sentence in sentences:
+    words = nltk.word_tokenize(sentence)
+    for word in words:
+        morph = MorphAnalyzer().parse(word)
+        wordCount = 0
+
+        for i in range(len(morph)):
+            if morph[i].normal_form == word.lower() or len(morph[i].normal_form)//len(word.lower()) < 3 and {'UNKN'} not in morph[i].tag:
+                # print(morph[i].normal_form, slovo.lower())
+                wordCount += 1
+        print("\n", word, wordCount)
+        print(morph)
+    print(words)
