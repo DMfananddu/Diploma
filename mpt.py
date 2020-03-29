8921232a3af219aa41051248902f198023749b13
dmfananddu <af0706@yandex.ru>
2020-03-29 22:20:08 +0300

    readme.md updated + nltk_parser.py finished + mpt.py created

--- mpt.py
+++ mpt.py
@@ -1,11 +1,10 @@
 import nltk_parser
 
 class Tree:
-	def __init__(self, kids, nextVar=None, pm=None):
+	def __init__(self, kids, nextVar=None):
 		self.kids = self.val = kids
 		self.next = nextVar
-        self.punctuation_mark = pm
-
+        
 test_sentence = ['Паша', ',', 'Саша', 'и', 'Вася', 'пошли', 'гулять', '.']
 
 def MPT_forming(sentence):
