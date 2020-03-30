5c09c813c8e1984020a11ec5120c0f05b7a49c12
dmfananddu <af0706@yandex.ru>
2020-03-30 17:11:03 +0300

    mpt.py finished (nonactual file, in further development will be used other files)

--- mpt.py
+++ mpt.py
@@ -1,11 +1,30 @@
-import nltk_parser
+# import nltk_parser
 
 class Tree:
-	def __init__(self, kids, nextVar=None):
-		self.kids = self.val = kids
-		self.next = nextVar
+	def __init__(self, value=None, nextVariant=None, kids=None):
+		self.value = value
+		self.brother = nextVariant
+		self.kids = kids
         
-test_sentence = ['Паша', ',', 'Саша', 'и', 'Вася', 'пошли', 'гулять', '.']
+test_sentence = [['Павел', 'Павлуша'], [','], ['Александр', 'Алекс'], ['и', 'а', 'о'], ['Василий'], ['пошли'], ['гулять'], ['.']]
 
 def MPT_forming(sentence):
-    t = Tree(Tree)
+	sl = len(sentence)
+	globalTree = Tree()
+	globalTree.kids = MPT_hierarchy_forming(globalTree.kids, sentence, 0)
+	return globalTree
+
+def MPT_hierarchy_forming(tree, sentence, idx):
+	tree = Tree()
+	tree.kids = Tree()
+	var_count = len(sentence[idx])
+	bufLevelTree = Tree(sentence[idx][-1], None, tree.kids)
+	for j in range(var_count-2, -1, -1):
+		bufLevelTree = Tree(sentence[idx][j], bufLevelTree, tree.kids)
+	tree = bufLevelTree
+	if idx != len(sentence)-1:
+		MPT_hierarchy_forming(tree.kids, sentence, idx+1)
+	return tree
+
+test_tree = MPT_forming(test_sentence)
+print(test_tree.kids.value, test_tree.kids.kids, test_tree.kids.brother)
