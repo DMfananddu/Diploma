b76dd1185b780448517d24bf7a3aabc0dac7ffc8
dmfananddu <af0706@yandex.ru>
2020-04-14 21:37:01 +0300

    conj_finding.py (finding all conjs finished)

--- conj_finding.py
+++ conj_finding.py
@@ -1,44 +1,63 @@
 from reader import gettingData
 from nltk_parser import parsing
+from copy import deepcopy
 
 testText = gettingData()
 parsedTestText = parsing(testText)
 
+
+# получение списка союзов из специального файла в удобочитаемом виде
 all_conjs = []
 
 f = open("Conjunction_kinds.TXT", encoding="UTF-8").readlines()
+conj_kinds = []
+conj_kind_flag = False
 for line in f:
     elem = line.strip("\n").strip("\t")
+    if elem == "Подчинительные:":
+        conj_kind_flag = True
     if elem[-1] != ":":
         all_conjs.append(elem)
+    if not conj_kind_flag:
+        conj_kinds.append("Сочинительный")
+    else:
+        conj_kinds.append("Подчинительный")
+raw_conjs = deepcopy(all_conjs)
 all_conjs.sort()
 conj_count = len(all_conjs)
 
 for i in range(conj_count):
+    conj_kind = conj_kinds[raw_conjs.index(all_conjs[i])]
     if "...," in all_conjs[i]:
         conj1 = all_conjs[i].split("..., ")[0]
         conj2 = all_conjs[i].split("..., ")[1]
-        all_conjs[i] = [conj1.split(), conj2.split()]
+        all_conjs[i] = [conj1.split(), conj2.split(), conj_kind]
     else:
-        all_conjs[i] = [all_conjs[i].split(), ""]
+        all_conjs[i] = [all_conjs[i].split(), "", conj_kind]
 
 # print(all_conjs)
 
 def conjFinding(parsedText):
+    whole_text_pc = []
     for p in parsedText["paragraphs"]:
         for s in p["sentences"]:
+            # список всех возможных вариантов любых союзов, 
+            # которые могут начинаться на какой-либо лексеме
             curr_conjs = []
             for l in s["lexems"]:
+                # все лексемы к нижнему регистру, чтобы "Чем" и "чем" были одинаковыми лексемами 
                 wanted_lexem = l["lexem"].lower()
+                # все союзы, которые могут начинаться на данной лексеме
                 possible_conjs = []
                 for conj in all_conjs:
                     if conj[0][0] == wanted_lexem:
                         possible_conjs.append(conj)
                 curr_conjs.append(possible_conjs)
-            lexem_count = len(curr_conjs) # кол-во лексем в предложении
+            # кол-во лексем в предложении
+            lexem_count = len(curr_conjs)
             # список вариантов данного союза
-            # он должен заполнятсья только теми составными союзами
-            # и индексами начала этих союзов в предложении,
+            # он должен заполнятсья только теми видами союзов, союзами и
+            # индексами начала этих союзов в предложении,
             # которые реально есть в данном предложении
             possible_conjs = []
             for i in range(lexem_count):
@@ -51,6 +70,8 @@ def conjFinding(parsedText):
                     for j in range (len_сvl):
                         # рассматриваемый вариант союза
                         wanted_conj = curr_conjs[i][j][0]
+                        # рассматриваемый вид союза
+                        wanted_conj_kind = curr_conjs[i][j][2]
                         # длина союза в лексемах
                         len_wanted_conj1 = len(wanted_conj)
                         # если длина предполагаемого союза не выводит за пределы предложения, 
@@ -59,8 +80,9 @@ def conjFinding(parsedText):
                             for k in range(1, len_wanted_conj1):
                                 if s["lexems"][i+k]["lexem"] != wanted_conj[k]:
                                     break
-                            else:
-                                possible_conjs.append([[i, wanted_conj]])
+                            else: # нашли нужный союз
+                                possible_conjs.append([i, wanted_conj_kind, wanted_conj])
+                                # отдельный поиск второй части раздельно-составного союза по тому же плану
                                 if curr_conjs[i][j][1] != '':
                                     wanted_conj = curr_conjs[i][j][1]
                                     len_wanted_conj2 = len(wanted_conj)
@@ -74,10 +96,10 @@ def conjFinding(parsedText):
                                                 if s["lexems"][k+m]["lexem"] != wanted_conj[m]:
                                                     break
                                             else:
-                                                possible_conjs[-1].append([k, wanted_conj])
+                                                possible_conjs[-1].append([k, wanted_conj_kind, wanted_conj])
                                                 k += 1
-                                print(possible_conjs)
-                            
-                            
+            whole_text_pc.append(possible_conjs)
+    return whole_text_pc
+
 # testing
-conjFinding(parsedTestText)
+print(conjFinding(parsedTestText))
