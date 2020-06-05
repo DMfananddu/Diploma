6e414957980b16a3684ad39cf2f4c92dc3d82440
dmfananddu <af0706@yandex.ru>
2020-06-05 19:51:04 +0300

    rpz

--- 1.py
+++ 1.py
@@ -1,3 +1,81 @@
+class Vertex:
+    def __init__(self, word=None, tag=None, attribute=None):
+        # атрибуты
+        self.word = word # само слово
+        self.attribute = attribute # название члена предложения
+        self.tag = tag # ТЭГ (OpencorporaTag('PRTS,perf,past,pssv masc,sing'))
+
+class MetaVertex:
+    def __init__(self, word=None, tag=None, attribute=None,
+                 vertices=None, meta_vertices=None, edges=None, meta_edges=None):
+        # атрибуты
+        self.word = word # само слово
+        self.attribute = attribute # название члена предложения
+        self.tag = tag # ТЭГ (OpencorporaTag('PRTS,perf,past,pssv masc,sing'))
+        # внутренние элементы
+        self.vertices = vertices # набор вершин метавершины (list адресов)
+        self.meta_vertices = meta_vertices # набор метавершин метавершины (list адресов)
+        self.edges = edges # набор ребер метавершины (list адресов)
+        self.meta_edges = meta_edges # набор метаребер метаграфа (list адресов)
+
+class Edge:
+    def __init__(self, start_vertex=None, end_vertex=None, conjuction=None, punctuation_mark=None):
+        self.start_vertex = start_vertex # адрес исходной вершины
+        self.end_vertex = end_vertex # адрес конечной вершины
+        self.edge_orientation = False # направленность вершины
+        # attributes
+        self.conjuction = conjuction # союз (в неизменном виде элемента поступившего массива)
+        self.punctuation_mark = punctuation_mark # знак препинания (просто лексема)
+
+class MetaGraph:
+    def __init__(self, sentence=None, paragraph_number=None, sentence_number=None,
+                 vertices=None, meta_vertices=None, edges=None, meta_edges=None):
+        # атрибуты
+        self.sentence = sentence # текст предложения
+        self.paragraph_number = paragraph_number # номер параграфа в тексте
+        self.sentence_number = sentence_number # номер предложения в параграфе
+        # внутренние элементы
+        self.vertices = vertices # набор вершин метаграфа (list адресов)
+        self.meta_vertices = meta_vertices # набор метавершин метаграфа (list адресов)
+        self.edges = edges # набор ребер метаграфа (list адресов)
+        self.meta_edges = meta_edges # набор метаребер метаграфа (list адресов)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from nltk import tokenize
 from rusenttokenize import ru_sent_tokenize
 testText = "Я: она, ты."
